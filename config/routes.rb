6a21ca6eb2ff61ef16324db4daceb4ae3ce470f6
rpiflv <rpiflv@gmail.com>
2022-12-06 22:05:09 +0900

    new_post view

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,11 @@ Rails.application.routes.draw do
   #creating the route
   resources :posts
   root "posts#index" # this is setting the homeoage '/' to 'posts'
-  
+
+
+  Rails.application.routes.draw do 
+    get '/articles', to: "articles#index"
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end
