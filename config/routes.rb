7a8970aa6dd690fc68b6fd8f74054de46626f02a
rpiflv <rpiflv@gmail.com>
2022-12-07 19:45:34 +0900

    add delete feat

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  #creating the route
   resources :posts
   root "posts#index" # this is setting the homeoage '/' to 'posts'
 
-
-  Rails.application.routes.draw do 
-    get '/articles', to: "articles#index"
-  end
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
