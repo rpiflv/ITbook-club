6bdc3760915a776682ebd8babf891d2919d720cb
rpiflv <rpiflv@gmail.com>
2022-12-07 19:27:50 +0900

    add create, edit and update feat

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
 
     def index 
+        @posts = Post.all.order("created_at DESC")
     end
 
     def new
@@ -15,6 +16,23 @@ class PostsController < ApplicationController
         else
             render 'new'
         end
+    end
+
+    def show
+        @post = Post.find(params[:id])
+    end
+
+    def update
+        @post = Post.find(params[:id])
+        if @post.update(post_params)
+        redirect_to @post    
+        else
+            render 'edit'
+        end
+    end
+
+    def edit
+        @post = Post.find(params[:id])
     end
 
     private 
